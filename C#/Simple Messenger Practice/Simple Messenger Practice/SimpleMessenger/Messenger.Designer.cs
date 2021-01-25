82dec42d50b39749bf1ede9c5c8d95e23ca7e1b1
unknown <000731khs@naver.com>
2021-01-25 21:06:57 +0900

    C#_TCP_Async_Client

---
+++ C#/Simple Messenger Practice/Simple Messenger Practice/SimpleMessenger/Messenger.Designer.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SimpleMessenger
+{
+    partial class Messenger
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect1 = new System.Windows.Forms.Button();
+            this.outLabel = new System.Windows.Forms.TextBox();
+            this.btn_a = new System.Windows.Forms.RadioButton();
+            this.btn_b = new System.Windows.Forms.RadioButton();
+            this.connect_string = new System.Windows.Forms.TextBox();
+            this.input_button = new System.Windows.Forms.Button();
+            this.input_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // connect1
+            // 
+            this.connect1.Location = new System.Drawing.Point(805, 44);
+            this.connect1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.connect1.Name = "connect1";
+            this.connect1.Size = new System.Drawing.Size(86, 29);
+            this.connect1.TabIndex = 0;
+            this.connect1.Text = "접 속";
+            this.connect1.Click += new System.EventHandler(this.connect1_Click);
+            // 
+            // outLabel
+            // 
+            this.outLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.outLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outLabel.Location = new System.Drawing.Point(11, 125);
+            this.outLabel.Multiline = true;
+            this.outLabel.Name = "outLabel";
+            this.outLabel.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outLabel.Size = new System.Drawing.Size(880, 297);
+            this.outLabel.TabIndex = 0;
+            this.outLabel.TextChanged += new System.EventHandler(this.outLabel_TextChanged);
+            // 
+            // btn_a
+            // 
+            this.btn_a.Location = new System.Drawing.Point(750, 25);
+            this.btn_a.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_a.Name = "btn_a";
+            this.btn_a.Size = new System.Drawing.Size(58, 30);
+            this.btn_a.TabIndex = 1;
+            this.btn_a.Text = "A";
+            // 
+            // btn_b
+            // 
+            this.btn_b.Location = new System.Drawing.Point(750, 63);
+            this.btn_b.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_b.Name = "btn_b";
+            this.btn_b.Size = new System.Drawing.Size(58, 30);
+            this.btn_b.TabIndex = 2;
+            this.btn_b.Text = "B";
+            // 
+            // connect_string
+            // 
+            this.connect_string.BackColor = System.Drawing.SystemColors.Control;
+            this.connect_string.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.connect_string.Location = new System.Drawing.Point(518, 51);
+            this.connect_string.Multiline = true;
+            this.connect_string.Name = "connect_string";
+            this.connect_string.Size = new System.Drawing.Size(226, 25);
+            this.connect_string.TabIndex = 3;
+            this.connect_string.Text = "접속할 클라이언트를 선택하세요";
+            // 
+            // input_button
+            // 
+            this.input_button.Location = new System.Drawing.Point(743, 434);
+            this.input_button.Name = "input_button";
+            this.input_button.Size = new System.Drawing.Size(148, 70);
+            this.input_button.TabIndex = 4;
+            this.input_button.Text = "전 송";
+            this.input_button.UseVisualStyleBackColor = true;
+            this.input_button.Click += new System.EventHandler(this.input_button_Click);
+            // 
+            // input_box
+            // 
+            this.input_box.Location = new System.Drawing.Point(11, 434);
+            this.input_box.Multiline = true;
+            this.input_box.Name = "input_box";
+            this.input_box.Size = new System.Drawing.Size(718, 70);
+            this.input_box.TabIndex = 5;
+            // 
+            // Messenger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 560);
+            this.Controls.Add(this.input_box);
+            this.Controls.Add(this.input_button);
+            this.Controls.Add(this.connect_string);
+            this.Controls.Add(this.connect1);
+            this.Controls.Add(this.outLabel);
+            this.Controls.Add(this.btn_a);
+            this.Controls.Add(this.btn_b);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Messenger";
+            this.Text = "Simple Messenger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private EventHandler connect1_ClickAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private Button connect1;        //접속 버튼
+        private TextBox outLabel;         //출력창
+        private RadioButton btn_a;      // A
+        private RadioButton btn_b;      // B
+        private TextBox connect_string; //접속할 클라이언트를 선택하세요
+        private Button input_button;    // 전송 버튼
+        private TextBox input_box;      // 입력 창
+    }
+}
+
---
+++ C#/Simple Messenger Practice/Simple Messenger Practice/SimpleMessenger/Messenger.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SimpleMessenger
+{
+    public partial class Messenger : Form
+    {
+        /* [TODO]
+        * 클라이언트에 해당하는 폼입니다.
+        * 폼 디자이너를 이용하여 적절한 컨트롤들을 배치하고 여러 상호작용 이벤트들을 처리하여 
+        * 프로그램을 완성해보세요 :)
+        */
+        TcpClient client = null;
+        NetworkStream stream = null;
+        int port;
+        string name;
+
+        Socket mainSock;
+
+        public Messenger()
+        {
+            InitializeComponent();
+            mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+        }
+
+        private async Task Send(){
+            byte[] data = new byte[32768];
+            data = Encoding.ASCII.GetBytes(DateTime.Now.ToString("h:mm:ss") + " " +name + " : "  + input_box.Text);
+            
+            if (client == null) return;
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    while (stream == null)
+                    {
+                        await Task.Delay(10);
+                    }
+                    try
+                    {
+                        stream.Write(data, 0, data.Length);
+                        break;
+                    }
+                    catch (SocketException se) {break; }
+                }
+            });        
+        }
+        private async Task ReceiveMessage()
+        {
+            /* [TODO]
+             * client와 stream에 적절한 값을 입력하여 서버로부터 메세지를 받아옵니다.
+             * 해당 메소드를 사용하지 않아도 무방합니다.
+             */
+            if (client == null) return;
+            await Task.Run(async () =>
+            {
+                byte[] data = new byte[32768];
+                string responseData = "";
+                while (true)
+                {
+                    while (stream == null)
+                    {
+                        await Task.Delay(10);
+                    }
+                    try
+                    {
+                        int bytes = stream.Read(data, 0, data.Length);
+                        responseData = Encoding.Default.GetString(data, 0, bytes);
+                        if(name == "A")
+                        {
+                            if(outLabel.InvokeRequired){
+                                outLabel.BeginInvoke(new Action(()=> outLabel.Text += Environment.NewLine + ">>"  + responseData));
+                            }
+                            else{
+                                outLabel.Text += Environment.NewLine + ">>"  + responseData;
+                            }
+                        }    
+                        
+                        if (name == "B")
+                        {
+                            if(outLabel.InvokeRequired){
+                                outLabel.BeginInvoke(new Action(()=> outLabel.Text += Environment.NewLine + ">>" + responseData));
+                            }
+                            else{
+                                outLabel.Text += Environment.NewLine + ">>"  + responseData;
+                            }
+                        }
+                    }
+                    catch (SocketException se) { break; }
+                }
+            });
+        }
+
+        private async void connect1_Click(object sender, EventArgs e)
+        {
+            if(btn_a.Checked == true)
+            {
+                port = 5001;
+                name = "A";
+            }
+            if(btn_b.Checked == true)
+            {
+                port = 5002;
+                name = "B";
+            }
+
+            IPEndPoint clientAddress = new IPEndPoint(IPAddress.Parse(Get_MyIP()), port);
+            client = new TcpClient(clientAddress);
+            client.Connect(Get_MyIP(), 5050);
+            stream = client.GetStream();
+            
+
+            outLabel.Text += Environment.NewLine + ">>" + DateTime.Now.ToString("yyyy-MM-dd tt h:mm:ss") + "              " + name + "님께서 접속하셨습니다.";
+            
+
+            connect_string.Text = "접속 성공";
+            btn_a.Dispose();
+            btn_b.Dispose();
+            connect1.Dispose();
+            await ReceiveMessage();
+        }
+
+        public string Get_MyIP()
+        {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            string ClientIP = string.Empty;
+            for (int i = 0; i < host.AddressList.Length; i++)
+            {
+                if (host.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ClientIP = host.AddressList[i].ToString();
+                }
+            }
+            return ClientIP;
+        }
+
+
+        private async void input_button_Click(object sender, EventArgs e)
+        {
+            await Send();
+            outLabel.Text += "\r\n>>" + DateTime.Now.ToString("h:mm:ss ") + name + " : " + input_box.Text;
+            input_box.Text = "";
+        }
+
+
+        private void outLabel_TextChanged(object sender, EventArgs e)
+        {
+            outLabel.SelectionStart = outLabel.Text.Length;
+            outLabel.ScrollToCaret();
+        }
+
+        
+    }
+}
